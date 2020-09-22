b39f875250de833eb78d242e471e77f3558f28f5
Alexandra <sandra.larrosa88@gmail.com>
2020-09-22 10:54:51 +0200

    add animations under construction

---
+++ docs/precache-manifest.1dea6c568a74a7247f22b9fe5d89bd93.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13604c0160f4e082fc921a900ccb0756",
+    "url": "./index.html"
+  },
+  {
+    "revision": "270dd86b80b0404bfc41",
+    "url": "./static/css/main.0eb84acb.chunk.css"
+  },
+  {
+    "revision": "afc2d55c38d98025b771",
+    "url": "./static/js/2.66d1008e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.66d1008e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "270dd86b80b0404bfc41",
+    "url": "./static/js/main.f9c44d7b.chunk.js"
+  },
+  {
+    "revision": "b388ac9c340cc6e2817c",
+    "url": "./static/js/runtime-main.fa365cae.js"
+  },
+  {
+    "revision": "5c7368fbc1e92edc62ec081226376742",
+    "url": "./static/media/fondo-desktop.5c7368fb.png"
+  },
+  {
+    "revision": "0937ab4ae0a06e76b42f048c957780e4",
+    "url": "./static/media/logomotosolo.0937ab4a.png"
+  }
+]);
