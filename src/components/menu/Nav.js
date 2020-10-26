4e93266081323702b899159dc6d2319529376b5b
Alexandra <sandra.larrosa88@gmail.com>
2020-10-26 13:30:37 +0100

    add menu hidden and create state initial

--- src/components/menu/Nav.js
+++ src/components/menu/Nav.js
@@ -1,33 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './nav.scss';
 import iconMenu from './images/icons8_menu_48px_1.png';
 
 
 const Nav = () => {
+  const [showMenu, setShowMenu] = useState('hidden');
+
+    const showMenuNav = () => {
+        setShowMenu('')
+    }
+
     return (
         <>
-        <nav className='contentIconMenu'>
-                <img src={iconMenu} alt="Menú Navegación" className='iconMenu' />
-                <div className='navDesktop'>
-                    <li>
-                        ¿Quiénes somos?
-                    </li>
-                    <li>
-                        Rutas & Eventos
-                    </li>
-                    <li>
-                        Colaboradores
-                    </li>
-                    <li>
-                        Blog
-                    </li>
-                    <li>
-                        Store
-                    </li>
-                    <li>
-                        Login
-                    </li>
-                </div>
+        <div className='contentIconMenu'>
+                <img src={iconMenu} alt="Menú Navegación" className='iconMenu' onClick={showMenuNav}/>
+        </div>
+        <nav className={`navDesktop ${showMenu}`}>
+                <li className='listMenu'>
+                    ¿Quiénes somos?
+                </li>
+                <li className='listMenu'>
+                    Rutas & Eventos
+                </li>
+                <li className='listMenu'>
+                    Colaboradores
+                </li>
+                <li className='listMenu'>
+                    Blog
+                </li>
+                <li className='listMenu'>
+                    Store
+                </li>
+                <li className='listMenu'>
+                    Login
+                </li>
             </nav>
         </>
     );
