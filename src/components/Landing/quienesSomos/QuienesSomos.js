d4bafe6babfd4ec7f3c1c072aaab3b3e655c0c6c
Alexandra <sandra.larrosa88@gmail.com>
2020-10-27 13:25:48 +0100

    section finished mobile and tablet

--- src/components/Landing/quienesSomos/QuienesSomos.js
+++ src/components/Landing/quienesSomos/QuienesSomos.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import './quienesSomos.scss';
+import foto from './images/madrid.png';
 
 
 const QuienesSomos = () => {
     return (
         <section className='quienesSomos'>
-            <h3>HOLA</h3>
+            <h2 className='titleQuienesSomos'>¿QUIÉNES SOMOS?</h2>
+            <div className='contentQuienesSomos'>
+                <img className='imageQuienesSomos'src={foto} alt='fotos' />
+                <p className='textQuienesSomos'>Lorem fistrum benemeritaar tiene musho peligro hasta luego Lucas la caidita. Condemor torpedo la caidita papaar papaar amatomaa mamaar te va a hasé pupitaa torpedo. Ese hombree pecador ese pedazo de te va a hasé pupitaa fistro la caidita. Está la cosa muy malar amatomaa se calle ustée va usté muy cargadoo apetecan diodeno. Tiene musho peligro caballo blanco caballo negroorl condemor me cago en tus muelas sexuarl por la gloria de mi madre papaar papaar hasta luego Lucas por la gloria de mi madre jarl llevame al sircoo. Va usté muy cargadoo sexuarl de la pradera qué dise usteer ese pedazo de ese pedazo de sexuarl a peich. La caidita me cago en tus muelas la caidita diodenoo va usté muy cargadoo tiene musho peligro condemor mamaar caballo blanco caballo negroorl apetecan llevame al sircoo.</p>
+            </div>
+            <div className='contentQuienesSomos contentReverse'>
+                <img className='imageQuienesSomos'src={foto} alt='fotos' />
+                <p className='textQuienesSomos'>Lorem fistrum benemeritaar tiene musho peligro hasta luego Lucas la caidita. Condemor torpedo la caidita papaar papaar amatomaa mamaar te va a hasé pupitaa torpedo. Ese hombree pecador ese pedazo de te va a hasé pupitaa fistro la caidita. Está la cosa muy malar amatomaa se calle ustée va usté muy cargadoo apetecan diodeno. Tiene musho peligro caballo blanco caballo negroorl condemor me cago en tus muelas sexuarl por la gloria de mi madre papaar papaar hasta luego Lucas por la gloria de mi madre jarl llevame al sircoo. Va usté muy cargadoo sexuarl de la pradera qué dise usteer ese pedazo de ese pedazo de sexuarl a peich. La caidita me cago en tus muelas la caidita diodenoo va usté muy cargadoo tiene musho peligro condemor mamaar caballo blanco caballo negroorl apetecan llevame al sircoo.</p>
+            </div>
+            <div className='contentQuienesSomos'>
+                <img className='imageQuienesSomos'src={foto} alt='fotos' />
+                <p className='textQuienesSomos'>Lorem fistrum benemeritaar tiene musho peligro hasta luego Lucas la caidita. Condemor torpedo la caidita papaar papaar amatomaa mamaar te va a hasé pupitaa torpedo. Ese hombree pecador ese pedazo de te va a hasé pupitaa fistro la caidita. Está la cosa muy malar amatomaa se calle ustée va usté muy cargadoo apetecan diodeno. Tiene musho peligro caballo blanco caballo negroorl condemor me cago en tus muelas sexuarl por la gloria de mi madre papaar papaar hasta luego Lucas por la gloria de mi madre jarl llevame al sircoo. Va usté muy cargadoo sexuarl de la pradera qué dise usteer ese pedazo de ese pedazo de sexuarl a peich. La caidita me cago en tus muelas la caidita diodenoo va usté muy cargadoo tiene musho peligro condemor mamaar caballo blanco caballo negroorl apetecan llevame al sircoo.</p>
+            </div>
+
         </section>
     );
 };
