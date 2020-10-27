55ea7e809328ec84de3e886018bb222d836ce494
Alexandra <sandra.larrosa88@gmail.com>
2020-10-27 11:09:27 +0100

    add logos and titles components landing home

--- src/components/Landing/landingHome/Landing.js
+++ src/components/Landing/landingHome/Landing.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import './landing.scss';
+import logoMoto from './images/logomotosolo.png';
+import signRoad from './images/curvy-road-warning-sign black.png';
 
 
-const Landing = props => {
+const Landing = () => {
     return (
         <div className='landingHome'>
-            
+            <div className='contentLandingTitle'>
+                <h1 className='landingTitle'>Biker Girls Spain</h1>
+                <img className='landingLogo'src={logoMoto} alt='Logo Biker Girls' />
+                <h2 className='landingAdictas'>Adictas a la Libertad</h2>
+            </div>
+            <img className='contentLogoSign' src={signRoad} alt='Icon Señal de Carretera con Curvas' />
         </div>
     );
 };
