981ed382756d4a44be5bba57a24da99b8bbed4a4
Alexandra <sandra.larrosa88@gmail.com>
2020-10-29 10:59:09 +0100

    add links to RRSS

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -9,12 +9,44 @@ import youtube from './images/youtube.png';
 const Footer = () => {
     return (
         <footer className='footer'>
-            <div className='contentIcons'>
-                <img className='iconRRSS'src={instagram} alt='logo instagram' />
-                <img className='iconRRSS' src={facebook} alt='logo facebook' />
-                <img className='iconRRSS' src={twitter} alt='logo twitter' />
-                <img className='iconRRSS' src={youtube} alt='logo youtube' />
-            </div>
+            <ul className='contentIcons'>
+                <li>
+                    <a
+                        href='https://www.instagram.com/bikergirls_spain/?hl=es'
+                        target='_blank'
+                        rel='noopener noreferrer'
+                    >
+                        <img className='iconRRSS' src={instagram} alt='logo instagram' />
+                    </a>
+                </li>
+                <li>
+                    <a
+                        href='https://www.facebook.com/pages/category/Community/Biker-Girls-Spain-351410018921678/'
+                        target='_blank'
+                        rel='noopener noreferrer'
+                        >
+                        <img className='iconRRSS' src={facebook} alt='logo facebook' />
+                    </a>
+                </li>
+                <li>
+                <a
+                    href='https://twitter.com/Bikergirlspain'
+                    target='_blank'
+                    rel='noopener noreferrer'
+                >
+                        <img className='iconRRSS' src={twitter} alt='logo twitter' />
+                    </a>
+                </li>
+                <li>
+                <a
+                    href='https://www.youtube.com/channel/UCtp25WXC947AzrU2vg4aFSA/featured'
+                    target='_blank'
+                    rel='noopener noreferrer'
+                >
+                        <img className='iconRRSS' src={youtube} alt='logo youtube' />
+                    </a>
+                </li>
+            </ul>
             <p className='copyFooter'>&copy; 2020 BIKER GIRLS SPAIN</p>
         </footer>
     );
