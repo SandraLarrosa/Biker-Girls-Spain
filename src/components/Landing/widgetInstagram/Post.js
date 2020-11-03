864529260ecc5a4e0912638ba59baa6595c5adfa
Alexandra <sandra.larrosa88@gmail.com>
2020-11-03 18:04:49 +0100

    create card post and add style

--- src/components/Landing/widgetInstagram/Post.js
+++ src/components/Landing/widgetInstagram/Post.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import './post.scss';
 
 
-const Post = (props) => {
-    console.log(props);
+const Post = ({image, comments, likes, text, linkPost}) => {
     return (
-        <article>
+        <article className='cardPost'>
+            <div className='content'>
+                <a href={linkPost} target='_blank' rel="noopener noreferrer">
+                    <img className='cardPost__image' src={image} alt='imagen post instagram Biker girls spain' />
+                    <p className='cardPost__text'>{text}</p>
+                </a>
+            </div>
+            <div className='cardPost__contentSocial'>
+                <div>
+                    <i class="far fa-comment"></i>
+                    <p>{comments}</p>
+                </div>
+                <div>
+                    <i class="far fa-heart"></i>
+                    <p>{likes}</p>
+                </div>
+            </div>
             
         </article>
     );
