21987fa13459c5370c282f299fc190d4d6c6e32a
Alexandra <sandra.larrosa88@gmail.com>
2020-11-05 11:02:02 +0100

    create new component rutas y eventos

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter, Route, Switch } from 'react-router-dom';
 import '../stylesheets/App.scss';
 import Nav from './menu/Nav.js';
 import Landing from './Landing/landingHome/Landing';
+import Rutas from './rutas&eventos/Rutas';
 import Footer from './footer/Footer';
 
 
@@ -14,7 +15,9 @@ function App() {
       <Nav />
       <BrowserRouter>
         <Switch>
-          <Route exact path='/' component={Landing} />
+          {/* <Route exact path='/' component={Landing} /> */}
+          {/* <Route exact path='/RutasYEventos' component={Rutas} /> */}
+          <Rutas />
         </Switch>
       </BrowserRouter>
       <Footer /> 
---
+++ src/components/rutas&eventos/Rutas.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './rutas.scss';
+
+
+const Rutas = () => {
+    return (
+        <header className='sectionRutas'>
+            <p>Hola Mundo</p>
+        </header>
+    );
+};
+
+
+
+export default Rutas;
