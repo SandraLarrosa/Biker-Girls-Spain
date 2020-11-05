a0b60ddd7bd7baa3e66b899a204690cecd01250e
Alexandra <sandra.larrosa88@gmail.com>
2020-11-05 13:25:38 +0100

    add component list rutas

---
+++ src/components/rutas&eventos/ListRutas.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const ListRutas = (props) => {
+    return (
+        <section className='sectionListRutas'>
+            
+        </section>
+    );
+};
+
+
+
+export default ListRutas;
