61ff41d93521f2c2252ffa7d9a382396d485c70e
Alexandra <sandra.larrosa88@gmail.com>
2021-01-21 13:05:45 +0100

    add component underConstruction

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import './stylesheets/index.scss';
 import './stylesheets/reset.scss';
 import App from './components/App';
 import { HashRouter } from 'react-router-dom';
+import UnderConstruction from './components/UnderConstruction';
 
 ReactDOM.render(
   <React.StrictMode>
     <HashRouter>
-      <App />
+      <UnderConstruction />
     </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
